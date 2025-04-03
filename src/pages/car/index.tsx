66da95c15ed3883db9d355d732ea336bf20176df
Matheus Minetto <matheusminetto140502@gmail.com>
2025-04-03 11:40:52 -0300

    feat: pagina de detalhes

--- src/pages/car/index.tsx
+++ src/pages/car/index.tsx
@@ -1,5 +1,145 @@
 import { useEffect, useState } from "react";
+import { Container } from "../../components/container/index";
+import { useNavigate, useParams } from "react-router-dom";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { FaWhatsapp } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+interface CarProps {
+  id: string;
+  name: string;
+  model: string;
+  city: string;
+  year: string;
+  km: string;
+  description: string;
+  owner: string;
+  uid: string;
+  whatsapp: string;
+  images: ImageCarProps[];
+  created: string;
+  price: string;
+}
+
+interface ImageCarProps {
+  uid: string;
+  name: string;
+  url: string;
+}
 
 export function CarDetail() {
-  return <div className="">Página Detalhes</div>;
+  const [car, setCar] = useState<CarProps>();
+  const { id } = useParams();
+  const [sliderPerView, setSliderPerView] = useState<number>(2);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadCar() {
+      if (!id) {
+        return;
+      }
+      const docRef = doc(db, "cars", id);
+      getDoc(docRef).then((snapshot) => {
+        if (!snapshot.data()) {
+          navigate("/");
+        }
+
+        setCar({
+          id: snapshot.id,
+          name: snapshot.data()?.name,
+          year: snapshot.data()?.year,
+          city: snapshot.data()?.city,
+          km: snapshot.data()?.km,
+          model: snapshot.data()?.model,
+          uid: snapshot.data()?.uid,
+          description: snapshot.data()?.description,
+          images: snapshot.data()?.images,
+          owner: snapshot.data()?.owner,
+          whatsapp: snapshot.data()?.whatsapp,
+          created: snapshot.data()?.created,
+          price: snapshot.data()?.price,
+        });
+      });
+    }
+    loadCar();
+  }, [id]);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 720) {
+        setSliderPerView(1);
+      } else {
+        setSliderPerView(2);
+      }
+    }
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <Container>
+      {car && (
+        <Swiper
+          slidesPerView={sliderPerView}
+          pagination={{ clickable: true }}
+          navigation
+        >
+          {car?.images.map((image) => (
+            <SwiperSlide key={image.name}>
+              <img src={image.url} className="w-full h-96 object-cover " />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+
+      {car && (
+        <main className="w=full bg-white rounded-lg p-6 my-4">
+          <div className="flex flex-col sm:flex-row mb-4 items-center justify-between">
+            <h1 className="font-bold text-3xl text-black">{car?.name}</h1>
+            <h1 className="font-bold text-3xl text-black">{car?.price}</h1>
+          </div>
+          <p>{car?.model}</p>
+          <div className="flex w-full gap-6 my-4">
+            <div className="flex flex-col gap-4">
+              <div className="">
+                <p>Cidade</p>
+                <strong>{car?.city}</strong>
+              </div>
+              <div className="">
+                <p>Ano</p>
+                <strong>{car?.year}</strong>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <div className="">
+                <p>KM</p>
+                <strong>{car?.km}</strong>
+              </div>
+            </div>
+          </div>
+
+          <strong>Descrição</strong>
+          <p className="mb-4"> {car?.description}</p>
+
+          <strong> Telefone / WhatsApp </strong>
+          <p>{car?.whatsapp}</p>
+          <a
+            href={`https://api.whatsapp.com/send?phone=${car?.whatsapp}&text=Olá vi esse ${car?.name} no site WebCarros e fiquei interessado(a) `}
+            target="_blank"
+            className="bg-green-500 w-full text-white flex items-center justify-center gap-2 my-6 h-11 text-lg rounded-lg font-medium cursor-pointer"
+          >
+            Conversar com o vendedor
+            <FaWhatsapp size={26} color="#FFF" />
+          </a>
+        </main>
+      )}
+    </Container>
+  );
 }
